5554e59e805ad7b72f2afbab33e65eb2b6b462ba
Jack Webb <webbjt07@gmail.com>
2023-09-26 00:13:15 -0500

    basic UI added

---
+++ app/PlanView/page.tsx
@@ -0,0 +1,11 @@
+//page handels user login 
+import Link from "next/link"
+export default function PlanView() {
+  return (
+    <main>
+       
+            <p>Hello?</p>
+            <Link href={"/"}>GO Back</Link>
+    </main>
+  )
+}
