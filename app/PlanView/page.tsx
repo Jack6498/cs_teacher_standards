188f48485fc64e80c1d4bb2a3ddeac9b6de63d5c
Jack <webbjt07@gmail.com>
2023-10-02 00:09:27 -0400

    fixed more bugs

--- app/PlanView/page.tsx
+++ app/PlanView/page.tsx
@@ -5,44 +5,40 @@ import DatePicker from 'react-datepicker';
 import React, {useState} from "react";
 import dynamic from "next/dynamic";
 import 'react-quill/dist/quill.snow.css';
-import 'react-datepicker/dist/react-datepicker-cssmodules.css';
-
+import 'react-datepicker/dist/react-datepicker.css'; 
+const ReactQuill = dynamic(
+  ()=> {
+    return import('react-quill');
+  },
+  {ssr:false}
+);
 
 export default function PlanView() {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
-  const [pdfValue, setPdfValue] = useState("");
-  const ReactQuill = dynamic(
-    ()=> {
-      return import('react-quill');
-    },
-    {ssr:false}
-  );
-  
+  const [pdfValue, setPdfValue] = useState('');
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <div className="flex flex-row w-3/4 justify-between">
-        <div className="m-8 w-fit">
-          <Link href={"/"} className="">GO Back</Link>
-        </div>
+      <div className="relative w-3/4 items-center">
+          <Link href={"/"} className="absolute bg-white rounded-xl p-4 m-4 flex-none">GO Back</Link>
         <p className="text-3xl font-semibold m-8 float-right">Lesson Plan Submission</p>
       </div>
         <div className="w-3/4 bg-white rounded-lg">
           <form className="p-4 items-start">
             <label>Data Range:</label>
-            <DatePicker selected={startDate} onChange={() => setStartDate(new Date())} id="date"/>
+            <DatePicker selected={startDate} onChange={(date) => setStartDate(date!)} />
             <label>To:</label>
-            <DatePicker selected={endDate} onChange={() => setEndDate(new Date())} id="date"/>
+            <DatePicker selected={endDate} onChange={(date) => setEndDate(date!)} />
             <label htmlFor="CoursePicker">Course Name:</label>
             <select name="CoursePicker" defaultValue={"Select"} className="mt-1 block">
               <option value="Pre-Ap English">Pre-Ap English</option>
               <option value="AP/DC English">AP/DC English</option>
             </select>
-            <label htmlFor="nameInput">Teacher Name:</label>
-            <input name="nameInput"></input>
+            <label htmlFor="nameInput" className="mt-1 block">Teacher Name:</label>
+            <input name="nameInput" className="border border-slate-300 border-2 rounded-lg"></input>
             <label htmlFor="">Lesson Plan:</label>
-            <ReactQuill theme="snow" id = "pdf" onChange={(e)=> setPdfValue(e)}/>
+            <ReactQuill theme="snow" id = "pdf" value={pdfValue} onChange={(e)=> setPdfValue(e)}/>
             </form>
       </div>
     </main>
---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import React from 'react'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'WCSC Lesson Plan Manager v1.0',
+  description: 'Helps teachers submit and manage their lesson plans to ensure they meet state standards',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
